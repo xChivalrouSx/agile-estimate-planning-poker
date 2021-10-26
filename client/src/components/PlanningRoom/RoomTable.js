8aa7562fffdd1f188ac30b1c849ca793f021c0e3
Mert Çakar <mertcakar.es@gmail.com>
2021-10-27 02:25:44 +0300

    Table size is fixed.

--- client/src/components/PlanningRoom/RoomTable.js
+++ client/src/components/PlanningRoom/RoomTable.js
@@ -8,9 +8,11 @@ const RoomTable = ({ roomInfo }) => {
 				style={{
 					marginTop: "125px",
 					marginBottom: "125px",
+					width: "600px",
+					height: "250px",
 				}}
 				className="border border-dark rounded-pill position-relative
-								bg-success w-50 h-25 mx-auto
+								bg-success mx-auto
 								d-flex align-items-center justify-content-center"
 			>
 				<div class="position-absolute top-0 start-100 translate-middle badge">
